4117910d24e58ab97bacfc69d1e262b25ff236df
Kara Dees <Kara.dees2@gmail.com>
2020-04-08 06:58:40 -0400

    Added Decrement button

--- udacity-git-course/ClickCounter/ClickCounter/ViewController.swift
+++ udacity-git-course/ClickCounter/ClickCounter/ViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class ViewController: UIViewController {
     var count=0
     var label:UILabel!
+    var secondLabel:UILabel!
     
     
     override func viewDidLoad() {
@@ -24,6 +25,13 @@ class ViewController: UIViewController {
         view.addSubview(label)
         self.label = label
         
+        //MARK: Label 2
+        let secondLabel = UILabel()
+        secondLabel.frame = CGRect(x: 130, y: 130, width: 60, height: 60)
+        secondLabel.text = "0"
+        view.addSubview(secondLabel)
+        self.secondLabel = secondLabel
+        
         //MARK: Button
         let button = UIButton()
         button.frame = CGRect(x: 150, y: 250, width:60, height: 60)
@@ -31,12 +39,32 @@ class ViewController: UIViewController {
         button.setTitleColor(UIColor.blue, for: .normal)
         view.addSubview(button)
         
+        //MARK: Decrement Button
+        let decrementButton = UIButton()
+        decrementButton.frame = CGRect(x: 150, y: 300, width:100, height: 60)
+        decrementButton.setTitle("Decrement", for: .normal)
+        decrementButton.setTitleColor(UIColor.purple, for: .normal)
+        view.addSubview(decrementButton)
+        
         button.addTarget(self, action: #selector(ViewController.incrementCount), for: UIControl.Event.touchUpInside)
+        
+        decrementButton.addTarget(self, action: #selector(ViewController.decrementCount), for: UIControl.Event.touchUpInside)
         }
+    
+    
+    
      //MARK: Function
     @objc func incrementCount(){
         self.count += 1
         self.label.text = "\(self.count)"
+        self.secondLabel.text = "\(self.count)"
+    }
+        
+    //MARK: Function
+    @objc func decrementCount(){
+       self.count -= 1
+       self.label.text = "\(self.count)"
+       self.secondLabel.text = "\(self.count)"
         
     }
 
